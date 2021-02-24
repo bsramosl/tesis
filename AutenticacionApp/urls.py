2bb63357f9c0f6b0de01c0ca317977721d21824d
bsramosl <steeven10f@gmail.com>
2021-02-24 09:12:08 -0500

    24

---
+++ AutenticacionApp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from AutenticacionApp import views
+
+urlpatterns = [
+    path('',views.VistaRegistro name="VistaRegistro"),  
+    
+]
--- AutenticacionApp/views.py
+++ AutenticacionApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+
+class VistaRegistro(View):
+    
+    def get(self,request):
+        form = UserCreationForm()
+        return render(request,"TesisApp/registro.html",{'form':form})
+
+    def post(self,render):
+        return
