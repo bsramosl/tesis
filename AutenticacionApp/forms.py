ba185593ea9ec1491196c275988ab2625a3ea813
bsramosl <77085266+bsramosl@users.noreply.github.com>
2021-03-04 18:30:30 -0500

    0403

--- AutenticacionApp/forms.py
+++ AutenticacionApp/forms.py
@@ -50,6 +50,7 @@ class UsuForm(ModelForm):
             'username',
             'email',
             'first_name',
-            'last_name',            
+            'last_name',
+            'is_superuser'           
         )
 
